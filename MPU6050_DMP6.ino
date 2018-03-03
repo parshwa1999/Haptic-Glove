edd346d151b6043011712d0337956d19cf2ce19a
parshwa1999 <parshwa1999@gmail.com>
2018-03-03 13:42:05 +0530

    commit 1v6

--- MPU6050_DMP6.ino
+++ MPU6050_DMP6.ino
@@ -96,15 +96,5 @@ void loop() {
 
     // wait for MPU interrupt or extra packet(s) available
     while (!mpuInterrupt && fifoCount < packetSize) {
-        // other program behavior stuff here
-        // .
-        // .
-        // .
-        // if you are really paranoid you can frequently test in between other
-        // stuff to see if mpuInterrupt is true, and if so, "break;" from the
-        // while() loop to immediately process the MPU data
-        // .
-        // .
-        // .
     }
 
