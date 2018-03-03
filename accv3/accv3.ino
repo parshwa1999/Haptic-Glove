938a91b94181e2af5621732c9dc98479c3003ca2
parshwa1999 <parshwa1999@gmail.com>
2018-03-03 22:54:36 +0530

    5v1

--- accv3/accv3.ino
+++ accv3/accv3.ino
@@ -1,3 +1,4 @@
+
 //#include <Mouse.h>
 
 #include <Wire.h>
@@ -9,43 +10,40 @@ int16_t ax, ay, az, gx, gy, gz;
 int vx, vy;
 int a1,a2;
 
-void setup() {
+void setup(){
 
   Serial.begin(9600);
   Wire.begin();
   mpu.initialize();
   while (!mpu.testConnection()) { Serial.print("NOT Connected\n");  }
-   Serial.println(F("\nSend any character to begin DMP programming and demo: "));
+ //  Serial.println(F("\nSend any character to begin DMP programming and demo: "));
 /*    while (Serial.available() && Serial.read()); // empty buffer
     while (!Serial.available());                 // wait for data
     while (Serial.available() && Serial.read()); // empty buffer again
 */
-  Serial.print("Connected");
+  //Serial.print("Connected");
 }
 
 void loop() {
   mpu.getMotion6(&ax, &ay, &az, &gx, &gy, &gz);
 
 
-  vx = (gx+15)/150;
-  vy = -(gz-100)/150;
+  vx = (gy+350)/200;
+  vy = -(gz-100)/200;
 
-  Serial.print("gx = ");
-  Serial.print(ax-a1);
-  Serial.print(" | gz = ");
-  Serial.print(az-a2);
-
-  Serial.print("        | X = ");
+  Serial.print("\n");
   Serial.print(vx);
-  Serial.print(" | Y = ");
-  Serial.println(vy);
+  Serial.print(" ");
+  Serial.print(vy);
+ 
+/*  Serial.print("ax = ");
+  Serial.print(gy);
+  Serial.print(" | az = ");
+  Serial.print(gz);
+  Serial.print(" \n");
+*/
 
-
-  a1=ax;
-  a2=az;
-  delay(500);
-
-
+  delay(50);
 }
 
 
